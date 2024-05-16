9ccd94d7d6b66c9b3bdeb410d2169faa80ab3683
DaikyXendo <hizamaakechi19901205@gmail.com>
2024-05-16 09:04:19 +0700

    get tag type

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import re
 
 
-def main():
-    with open(os.getcwd() + "/src/main.py", "r") as file:
+def read_file(path):
+    with open(os.getcwd() + path, "r") as file:
         content = file.read()
         # Get content inside """
         pattern = r'UIML.*"""(.*)"""'
@@ -11,39 +11,44 @@ def main():
         matches = re.findall(pattern, content, re.DOTALL)
         text = " ".join(matches[0].split())
 
-        flag = "unknown"
-        tag = ""
-        text_length = len(text)
-        is_close = False
-        name = ""
-        for idx, char in enumerate(text):
-            next_char = text[idx + 1] if idx < text_length - 1 else None
-
-            if is_close:
-                is_close = False
-                continue
-
-            if flag == "unknown" and char == "<":
-                if next_char == "/":
-                    flag = "close"
-                else:
-                    flag = "open"
-
-            if flag != "unknown" and next_char == ">":
-                if char == "/":
-                    flag = "au_close"
-                    tag += "/>"
-                else:
-                    tag += char + ">"
-
-                print(flag, tag)
-                flag = "unknown"
-                tag = ""
-
-                is_close = True
-                continue
-
-            tag += char
+        return text
 
 
-main()
+def main():
+    text = read_file("/src/main.py")
+
+    flag = "unknown"
+    tag = ""
+    is_close = False
+    name = ""
+
+    text_length = len(text)
+    for idx, char in enumerate(text):
+        next_char = text[idx + 1] if idx < text_length - 1 else None
+
+        if is_close:
+            is_close = False
+            continue
+
+        if flag == "unknown" and char == "<":
+            flag = "close" if next_char == "/" else "open"
+
+        if flag != "unknown" and next_char == ">":
+            if char == "/":
+                flag = "au_close"
+                tag += "/>"
+            else:
+                tag += char + ">"
+
+            print(flag, tag)
+            flag = "unknown"
+            tag = ""
+
+            is_close = True
+            continue
+
+        tag += char
+
+
+if __name__ == "__main__":
+    main()
