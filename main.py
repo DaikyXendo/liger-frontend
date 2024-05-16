cc6c7984b1e493f445d5e4bcf63c1e09eb60f0cc
DaikyXendo <hizamaakechi19901205@gmail.com>
2024-05-16 11:39:45 +0700

    add command

--- main.py
+++ main.py
@@ -23,30 +23,42 @@ def main():
 
     tag = ""
     is_open_tag = False
+    command = ""
+    is_open_command = False
 
     for char in text:
         if char == "<":
             is_open_tag = True
+            is_open_command = False
             continue
 
         if is_open_tag:
             if char == ">":
                 is_open_tag = False
 
-                tag_type = None
-                if tag.startswith("/"):
-                    tag_type = "close"
-                elif tag.endswith("/"):
-                    tag_type = "au_close"
-                else:
-                    tag_type = "open"
+                tag_type = (
+                    "close"
+                    if tag.startswith("/")
+                    else "au_close"
+                    if tag.endswith("/")
+                    else "open"
+                )
 
-                name = tag.split(" ")[0]
+                name = tag.split(" ")[0].replace("/", "")
 
                 print(tag_type, name, "<" + tag + ">")
                 tag = ""
             else:
                 tag += char
+        else:
+            if char == ":":
+                name = command.strip().split(" ")[0]
+
+                print("command", name, command + ":")
+                command = ""
+
+            else:
+                command += char
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -5,8 +5,72 @@ UIML = """
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title></title>
+        <title label="Liger Frontend" />
         <link href="css/style.css" rel="stylesheet" />
     </head>
-    <box></box>
+    <box>
+        <!-- Inputs -->
+        <autocomplete />
+        <button />
+        <group-button />
+        <checkbox />
+        <radio />
+        <input />
+        <text-area />
+        <select />
+        <slider />
+        <switch />
+
+        <!-- Data Display -->
+        <avatar />
+        <badge />
+        <chart />
+        <chip />
+        <grid />
+        <list />
+        <table />
+        <tooltip />
+        <tree />
+        <progress />
+
+        <!-- Feedback -->
+        <alert />
+        <dialog />
+        <snackbar />
+        <toast />
+
+        <!-- Surface -->
+        <accordion />
+        <app-bar />
+
+        <-- Built-in -->
+        <color-picker />
+        <audio-player />
+        <video-player />
+        <text-editor />
+        <paint />
+        <document-viewer />
+
+        <!-- Navigation -->
+        <drawer />
+        <navigation />
+        
+        
+
+        <text label="Hello, World!" />
+        <p label="Hello, World!" />
+        <ver></ver>
+        <hor></hor>
+
+
+        if x == 1:
+            <text label="x = 1" />
+        elif x == 2:
+            <text label="x = 2" />
+        else:
+            <text label="other" />
+
+        for i in range(10):
+            <text label="Hello, World!" />
+    </box>
 """
